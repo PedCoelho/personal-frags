dd0f2c2516a9a014ebdbbbfa6afb703ba8b153c1
Pedro Coelho <tp.pedroh.silva@d0007azurecloudtotvscom.onmicrosoft.com>
2024-02-23 11:00:50 -0300

    feat(ci): remove test job

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientModule, AppModule],
+      providers: [provideMockStore()],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +20,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-frag'`, () => {
+  it(`should have as title 'personal-frags'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-frag');
+    expect(app.title).toEqual('personal-frags');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('personal-frag app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'personal-frag app is running!'
+    );
   });
 });
--- src/app/modules/shared/services/perfume-search.service.spec.ts
+++ src/app/modules/shared/services/perfume-search.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { PerfumeSearchModule } from '../components/perfume-search/perfume-search.module';
 import { PerfumeSearchService } from './perfume-search.service';
 
 describe('PerfumeSearchService', () => {
   let service: PerfumeSearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [PerfumeSearchModule],
+    });
     service = TestBed.inject(PerfumeSearchService);
   });
 
