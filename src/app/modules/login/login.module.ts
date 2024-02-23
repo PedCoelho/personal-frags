e54479a5b3970cb5ab56b46b9bd9bb30500d16dd
Pedro Coelho <tp.pedroh.silva@d0007azurecloudtotvscom.onmicrosoft.com>
2024-02-23 11:14:45 -0300

    feat(ci): update project deps and budget

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FirebaseUIModule } from 'firebaseui-angular';
 import { MaterialModule } from '../shared/material.module';
 import { LoginComponent } from './container/login.component';
 import { LoginRoutingModule } from './login-routing.module';
@@ -12,7 +11,6 @@ import { LoginRoutingModule } from './login-routing.module';
     ReactiveFormsModule,
     MaterialModule,
     LoginRoutingModule,
-    FirebaseUIModule,
   ],
   declarations: [LoginComponent],
   exports: [LoginComponent],
