5f106c60530cb67cadba07f16900d2f8e07a61ba
Pedro Coelho <tp.pedroh.silva@d0007azurecloudtotvscom.onmicrosoft.com>
2024-02-24 11:07:22 -0300

    feat(collection): add user sorting

--- src/app/modules/collection/components/collection-grid/collection-grid.component.ts
+++ src/app/modules/collection/components/collection-grid/collection-grid.component.ts
@@ -6,7 +6,7 @@ import { State } from 'app/+state/app.reducers';
 import { UserPerfume } from 'app/modules/collection/models/collection.models';
 import { SearchResult } from 'app/modules/shared/components/perfume-search/models/perfume-search.models';
 import { NotificationService } from 'app/modules/shared/services/notification.service';
-import { Subscription, finalize } from 'rxjs';
+import { Subscription, finalize, map } from 'rxjs';
 import { CollectionService } from '../../services/collection.service';
 @Component({
   selector: 'collection-grid',
@@ -33,6 +33,7 @@ export class CollectionGridComponent implements OnDestroy, OnInit {
 
   public drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.collection, event.previousIndex, event.currentIndex);
+    this.setUserSort();
   }
 
   public addToCollection(perfume: SearchResult) {
@@ -87,10 +88,60 @@ export class CollectionGridComponent implements OnDestroy, OnInit {
 
   public getCollection(): void {
     this.subs.push(
-      this.collectionService.getCollection().subscribe((data) => {
-        this.collection = data;
-        this.store.dispatch(updateCollection({ collection: this.collection }));
-      })
+      this.collectionService
+        .getCollection()
+        .pipe(map((data) => this.collectionSort(data)))
+        .subscribe((data) => {
+          this.collection = data;
+          this.store.dispatch(
+            updateCollection({ collection: this.collection })
+          );
+        })
     );
+  }
+
+  private setUserSort(): void {
+    //todo when userSort is added, show a way to reset user sort
+    localStorage.setItem(
+      'collection-sort',
+      this.collection.map((perfume, i) => perfume.id).join(',')
+    );
+  }
+
+  private getUserSort(): string[] | undefined {
+    const data = localStorage.getItem('collection-sort')?.split(',');
+    if (data?.length) return data;
+    else return undefined;
+  }
+
+  private collectionSort(data: UserPerfume[]): UserPerfume[] {
+    const companySort = (a: UserPerfume, b: UserPerfume) =>
+      b.company > a.company ? -1 : 1;
+
+    const nameSort = (a: UserPerfume, b: UserPerfume) =>
+      b.name > a.name ? -1 : 1;
+
+    const initialSort = data.sort(nameSort).sort(companySort);
+
+    const userSort = this.getUserSort();
+
+    return userSort ? this.applyUserSort(userSort, initialSort) : initialSort;
+  }
+  private applyUserSort(
+    userSort: string[],
+    initialSort: UserPerfume[]
+  ): UserPerfume[] {
+    const initialSortCopy = [...initialSort];
+    initialSortCopy.forEach((perfume) => {
+      const userSortIndex = userSort.indexOf(perfume.id);
+      if (userSortIndex !== -1)
+        moveItemInArray(
+          initialSortCopy,
+          initialSort.indexOf(perfume),
+          userSortIndex
+        );
+    });
+
+    return initialSortCopy;
   }
 }
